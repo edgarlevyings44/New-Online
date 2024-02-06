f0c12c12677b1449089609be2dd67f5dab6069e8
Levy <edgarlevy81@gmail.com>
2024-02-06 22:03:57 +0300

    link:myorder adjusted

--- client/src/Components/Homepage/MyOrders.js
+++ client/src/Components/Homepage/MyOrders.js
@@ -6,7 +6,7 @@ function MyOrders() {
     const [record, setRecord] = useState([])
 
     useEffect(() => {
-        fetch('https://dedanite-online.onrender.com/orders')
+        fetch('https://dedanite-online.onrender.com/myorders')
         .then((response) => response.json())
         .then((data) => {
             setRecord(data)
