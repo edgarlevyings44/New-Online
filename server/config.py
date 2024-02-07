07cd2ebc722f2b04dc319f6d216469089e0e06e3
Levy <edgarlevy81@gmail.com>
2024-02-07 21:00:31 +0300

    Create config file

---
+++ server/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
