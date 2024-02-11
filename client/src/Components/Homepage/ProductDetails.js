d9f5ba34a8646e9f7580361050ecae541ecffeed
Levy <edgarlevy81@gmail.com>
2024-02-11 17:27:18 +0300

    Product Dets CSS

--- client/src/Components/Homepage/ProductDetails.js
+++ client/src/Components/Homepage/ProductDetails.js
@@ -10,10 +10,7 @@ const ProductDetails = ({ match }) => {
     fetch(`https://dedanite-online.onrender.com/products/${id}`)
     
       .then((response) => response.json())
-      .then((data) => {
-        setProduct(data);
-        console.log(data.details);
-      })
+      .then((data) => setProduct(data))
       .catch((error) => console.error('Error fetching product details:', error));
   }, [id]);
 
@@ -23,16 +20,27 @@ const ProductDetails = ({ match }) => {
 
   return (
     <div className='container'>
-      <h1>{product.name}</h1>
-      <p>${product.price}</p>
-      <img src={product.imageurl} alt={product.name} />
-      <p>Category: {product.category}</p>
-      <p>Quantity: {product.quantity}</p>
-      <p>Weight: {product.weight}</p>
-      <p>Details: {product.details}</p>
+      <div className='product-card'>
+        <img className='product-image' src={product.imageurl} alt={product.name} />
+          <div className='text-details'>
+            <h1 className='catchy-heading'>{product.name}</h1>
+            <p><strong>Price:</strong><span>${product.price}</span></p>
+            <p><strong>Category:</strong><span>{product.category}</span></p>
+            <p><strong>Quantity:</strong><span>{product.quantity}</span></p>
+            <p><strong>Weight:</strong><span>{product.weight}</span></p>
+            <div className="stars">
+              {[...Array(5)].map((_, index) => (
+                <span key={index} className="inactive-star">&#9734;</span>
+              ))}
+            </div>
+            <div className="description">
+              <h2>Description:</h2>
+              <p>Discover the excellence of our premium supermarket product, a delightful blend of quality and taste. Our carefully selected ingredients ensure freshness and flavor with every bite.</p>
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default ProductDetails;
-
